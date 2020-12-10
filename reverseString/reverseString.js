c01b4f87cd6d0ab53b9210866cd092e6a1e07411
Dan Wells <wellsdanielj@gmail.com>
2020-12-09 20:53:47 -0500

    Completed reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let split = string.split('');
+  let reverse = '';
 
+  for (i = (split.length - 1); i >= 0; i--) {
+    reverse = reverse.concat(split[i]);
+  }
+  return reverse;
 }
 
 module.exports = reverseString
