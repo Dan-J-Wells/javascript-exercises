44ee5f3371af70a7d7030218c943078a3dd0556c
Dan Wells <wellsdanielj@gmail.com>
2020-12-09 20:42:10 -0500

    Completed repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeat) {
+  if (repeat < 0) {
+    return 'ERROR';
+  }
+  else {
+    let repString = '';
+    for (let i = 0; i < repeat; i++) {
+      repString += string;
+    }
+    return repString;
+  }
 }
 
 module.exports = repeatString
