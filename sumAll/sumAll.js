b1b869ee68dbfc2db98c876859ee8e965d450afc
Dan Wells <wellsdanielj@gmail.com>
2020-12-10 13:25:54 -0500

    Completed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  let value = 0;
 
+  if (typeof(a) !== 'number' || typeof(b) !== 'number') {
+    return 'ERROR';
+  }  
+  else if (a < 0 || b < 0) {
+    return 'ERROR';
+  }
+  else {
+    for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+      value += i;
+    }
+  }
+
+  return value;
 }
 
 module.exports = sumAll
