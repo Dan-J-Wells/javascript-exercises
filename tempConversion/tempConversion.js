cb72b079c798fe4815cc9948adc7c0874f6e49ad
Dan Wells <wellsdanielj@gmail.com>
2020-12-10 13:49:36 -0500

    Completed tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let convert = (temp - 32) * (5 / 9);
+  return +convert.toFixed(1);
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let convert = ((9 / 5) * temp) + 32;
+  return +convert.toFixed(1);
 }
 
 module.exports = {
