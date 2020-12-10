e9f4261c7a83d5a2c253770a46513b7d4cc558ec
Dan Wells <wellsdanielj@gmail.com>
2020-12-10 13:00:14 -0500

    Completed removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
+let args = Array.from(arguments);
+let arr = args[0];
+args.shift();
 
+for (let i = 0; i < args.length; i++) {
+  let index = arr.indexOf(args[i]);
+  if (index === -1) {continue;}
+  else {arr.splice(index, 1);}
+}
+
+return arr;
 }
 
 module.exports = removeFromArray
