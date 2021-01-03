24b4e4667ea5d8675a958dfab25b5b09744852a0
Dan Wells <wellsdanielj@gmail.com>
2021-01-03 16:34:38 -0500

    Completed Exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,30 @@
-function add () {
-	
+function add (a,b) {
+	return +a + +b;
 }
 
-function subtract () {
-	
+function subtract (a,b) {
+	return +a - +b;
 }
 
-function sum () {
-	
+function sum (arr) {
+	return arr.reduce((result, term) => result + term, 0);
 }
 
-function multiply () {
-	
+function multiply (arr) {
+	return arr.reduce((result, term) => result * term, 1);
 }
 
-function power() {
-	
+function power(a,b) {
+	return a ** b;
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if (num === 0) return 1;
+	let total = 1;
+	for (let i = 1; i <= num; i++) {
+		total *= i;
+	}
+	return total;
 }
 
 module.exports = {
