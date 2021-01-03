1555e768b57a1a2f701314da63841eb0cfa0eb8e
Dan Wells <wellsdanielj@gmail.com>
2021-01-03 18:08:32 -0500

    Completed Exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+  const initialString = str.toLowerCase().replace(/[^a-z]/g, '');
+  const newString = initialString.split('').reverse().join('');
+  return (initialString === newString) ? true : false;
 }
 
 module.exports = palindromes
