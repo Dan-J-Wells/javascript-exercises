8544721285febd26dfe546a250b9c558cb20b66e
Dan Wells <wellsdanielj@gmail.com>
2021-02-15 17:31:56 -0500

    Completed Exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+  //Ensuring positive num or numerical string
+  const num = (isNaN(input)) ? +input
+      : (Math.sign(input) < 0) ? "OOPS"
+      : input;
 
+  if (isNaN(num)) return num;
+
+  //Fibonacci generator
+  if(num <= 2) return 1;
+
+  let sequence = [1, 1];
+  for (let i = 2; i < num; i++) {
+    const nextVal = sequence[i - 2] + sequence[i - 1];
+    sequence.push(nextVal);
+  }
+  return sequence[num - 1];
 }
 
 module.exports = fibonacci
