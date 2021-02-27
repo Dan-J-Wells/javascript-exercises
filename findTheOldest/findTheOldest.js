f6a29d1e94847bf51b1e472a3c2798490e174ed4
Dan Wells <wellsdanielj@gmail.com>
2021-02-27 15:48:56 -0500

    COmpleted Exerceises

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+  function reducer(accumulator, currentValue) {
+    const today = new Date();
 
+    //age of current object
+    const currentAge = ("yearOfDeath" in currentValue) ? 
+    currentValue.yearOfDeath - currentValue.yearOfBirth : 
+    today.getFullYear() - currentValue.yearOfBirth;
+
+    //age of current oldest object
+    const currentOldest = ("yearOfDeath" in accumulator) ? 
+    accumulator.yearOfDeath - accumulator.yearOfBirth : 
+    today.getFullYear() - accumulator.yearOfBirth;
+
+    return (currentOldest > currentAge) ? accumulator : currentValue;
+  }
+  return people.reduce(reducer);
 }
 
 module.exports = findTheOldest
